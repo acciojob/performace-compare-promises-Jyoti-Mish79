ee761a484bf84255873e47aea717d544022c613d
Jyoti-Mish79 <jyotimishra2996@gmail.com>
2023-03-16 09:50:58 +0530

    solr

--- script.js
+++ script.js
@@ -12,4 +12,36 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
 
-// You can write your code here
+// Function to fetch data from a single API
+const fetchData = (url) => {
+  return fetch(url)
+    .then(response => response.json())
+    .catch(error => console.log(error));
+}
+
+// Measure time taken to fetch data using Promise.all
+const measurePromiseAll = () => {
+  const startTime = performance.now();
+  Promise.all(apiUrls.map(fetchData))
+    .then(data => {
+      const endTime = performance.now();
+      const timeTaken = (endTime - startTime).toFixed(2);
+      document.getElementById('output-all').innerHTML = timeTaken;
+    });
+}
+
+// Measure time taken to fetch data using Promise.any
+const measurePromiseAny = () => {
+  const startTime = performance.now();
+  Promise.any(apiUrls.map(fetchData))
+    .then(data => {
+      const endTime = performance.now();
+      const timeTaken = (endTime - startTime).toFixed(2);
+      document.getElementById('output-any').innerHTML = timeTaken;
+    });
+}
+
+// Call the two functions to measure the time taken
+measurePromiseAll();
+measurePromiseAny();
+
